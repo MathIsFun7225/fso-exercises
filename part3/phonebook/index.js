215eb8d9e7a961bb92c1358e820644cd7d3ffd25
Sam Memon <mustafalamemon@gmail.com>
2026-01-03 17:04:56 -0500

    exercise 3.5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,24 +5,24 @@ app.use(express.json())
 
 let persons = [
   { 
-    "id": "1",
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+    'id': '1',
+    'name': 'Arto Hellas', 
+    'number': '040-123456'
   },
   { 
-    "id": "2",
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+    'id': '2',
+    'name': 'Ada Lovelace', 
+    'number': '39-44-5323523'
   },
   { 
-    "id": "3",
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+    'id': '3',
+    'name': 'Dan Abramov', 
+    'number': '12-43-234345'
   },
   { 
-    "id": "4",
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
+    'id': '4',
+    'name': 'Mary Poppendieck', 
+    'number': '39-23-6423122'
   }
 ]
 
@@ -50,6 +50,24 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(p => p.id !== id)
 
   response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+
+  const newPerson = {
+    'id': Math.floor(Math.random() * Math.pow(2, 32)).toString(),
+    'name': body.name,
+    'number': body.number
+  }
+  persons.push(newPerson)
+  response.json(newPerson)
 })
 
 const PORT = 3001
